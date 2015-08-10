951eb763a88360a0b57fcbcaca24a9ec3af3b4c5
Tomi Järvi <tomi.jarvi@anders.fi>
2015-08-10 15:05:24 +0300

    Added JS packages for Gulp.

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp');
+var jsmin = require('gulp-jsmin');
+var rename = require('gulp-rename');
+var filter = require('gulp-filter');
+var bowerSrc = require('gulp-bower-src');
+var uglify = require('gulp-uglify');
