2c08f2191a377c918e3e5cb33d1238d92ca773b8
Tomi Järvi <tomi.jarvi@anders.fi>
2015-08-10 15:58:24 +0300

    Refactored gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,39 @@
 // Variables
-var debug = false;
-var dest = 'dist/';
+var debug = false
+var dest = 'dist/'
 
 // Gulp
-var gulp = require('gulp');
+var gulp = require('gulp')
 
 var plugins = require("gulp-load-plugins")({
     pattern: ['gulp-*', 'gulp.*', 'main-bower-files'],
     replaceString: /\bgulp[\-.]/
-});
+})
 
 gulp.task('clean-all', function() {
     gulp.src(dest)
-        .pipe(plugins.clean());
-});
+        .pipe(plugins.clean())
+})
 
 gulp.task('bower', function() {
     plugins.bower()
-    .pipe(gulp.dest('lib/'))
-});
+})
 
 gulp.task('default', ['clean-all'], function () {
     gulp.src(plugins.mainBowerFiles())
         .pipe(plugins.filter('*.js'))
         .pipe(plugins.concat('main.js'))
         .pipe(plugins.uglify())
-        .pipe(gulp.dest(dest + 'js'));
-});
+        .pipe(gulp.dest(dest + 'js'))
+})
 
 gulp.task('debug', ['clean-all'], function () {
     gulp.src(plugins.mainBowerFiles())
         .pipe(plugins.filter('*.js'))
         .pipe(plugins.uglify())
-        .pipe(gulp.dest(dest + 'js'));
-});
+        .pipe(gulp.dest(dest + 'js'))
+})
 
 gulp.task('watch', function () {
 
-});
+})
