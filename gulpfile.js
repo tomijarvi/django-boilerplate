b39d5c6b24c0eac7a9751a5bf6048ffc3f4c0d7c
Tomi Järvi <tomi.jarvi@anders.fi>
2015-08-11 14:51:26 +0300

    Coded javascript process..

--- gulpfile.js
+++ gulpfile.js
@@ -25,13 +25,13 @@ gulp.task('clean-all', ['clean-temp', 'clean-static', 'clean-inject'], function(
 })
 
 gulp.task('clean-temp', function() {
-    return gulp.src([tmp])
-        .pipe(plugins.clean())
+    return gulp.src('./tmp')
+        .pipe(plugins.clean({force: true}))
 })
 
 gulp.task('clean-static', function() {
-    return gulp.src([stat])
-        .pipe(plugins.clean())
+    return gulp.src('./static')
+        .pipe(plugins.clean({force: true}))
 })
 
 gulp.task('clean-inject', function() {
@@ -41,27 +41,26 @@ gulp.task('clean-inject', function() {
 })
 
 // Scripts
-gulp.task('build-js', ['clean-temp', 'build-src-js', 'build-vendor-js'], function() {
-
+gulp.task('build-js', ['clean-temp', 'build-app-js', 'build-vendor-js'], function() {
 })
 
-gulp.task('build-src-js', ['clean-temp'], function() {
+gulp.task('build-app-js', ['clean-temp'], function() {
     if(debug) {
-        gulp.src(src + 'js/*.js', { base: src + 'js/' })
-            .pipe(gulp.dest(tmp + 'js'))
+        gulp.src('./src/js/**/*.js', { base: src + 'js/' })
+            .pipe(gulp.dest('./tmp/js/app'))
     }
     else {
-        gulp.src(src + '**/*.js')
-            .pipe(plugins.concat('current.min.js'))
+        gulp.src('./src/js/**/*.js')
+            .pipe(plugins.concat('app.min.js'))
             .pipe(plugins.uglify())
-            .pipe(gulp.dest(tmp + 'js'))
+            .pipe(gulp.dest('./tmp/js'))
     }
 })
 
 gulp.task('build-vendor-js', ['clean-temp'], function() {
     if(debug) {
         return gulp.src(bowerMainJavaScriptFiles.normal)
-            .pipe(gulp.dest(tmp + 'js'))
+            .pipe(gulp.dest(tmp + 'js/vendor'))
     }
     else {
         return plugins.merge2(
@@ -82,8 +81,9 @@ gulp.task('copy-static', ['build-js'], function() {
 
 // Inject
 gulp.task('inject', ['clean-inject', 'copy-static'], function() {
+    jsFiles = [stat + 'js/vendor/*.js', stat + 'js/app/*.js']
     return gulp.src(templates + '**/*.html')
-      .pipe(plugins.inject(gulp.src(stat + '**/*.js', {read: false}), {relative: false}))
+      .pipe(plugins.inject(gulp.src(jsFiles, {read: false}), {relative: false}))
       .pipe(gulp.dest(templates))
 })
 
@@ -93,7 +93,7 @@ gulp.task('default', ['clean-all'], function () {
 })
 
 gulp.task('debug', ['clean-all'], function () {
-    debug = true;
+    debug = true
     gulp.start('build-js', 'inject')
 })
 
