2730bc78948af3adfe53393088ce8e25a067918f
Tomi Järvi <tomi.jarvi@anders.fi>
2015-08-11 15:42:41 +0300

    Added font copy.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,12 @@
 // Variables
 var debug = false
-var stat = 'static/'
-var tmp = 'tmp/'
-var src = 'src/'
 var templates = 'templates/'
 
 // Gulp
 var gulp = require('gulp')
 
 var plugins = require("gulp-load-plugins")({
-    pattern: ['gulp-*', 'gulp.*', 'bower-main', 'merge2'],
+    pattern: ['gulp-*', 'gulp.*', 'main-bower-files', 'bower-main', 'merge2'],
     replaceString: /\bgulp[\-.]/
 })
 
@@ -35,7 +32,7 @@ gulp.task('clean-static', function() {
 })
 
 gulp.task('clean-inject', function() {
-    return gulp.src(templates + '**/*.html')
+    return gulp.src('./templates/**/*.html')
         .pipe(plugins.replace(/<!-- (.*?):js -->([\S\s.]*)<!-- endinject -->/g, '<!-- $1:js -->\n  <!-- endinject -->'))
         .pipe(gulp.dest(templates))
 })
@@ -46,7 +43,7 @@ gulp.task('build-js', ['clean-temp', 'build-app-js', 'build-vendor-js'], functio
 
 gulp.task('build-app-js', ['clean-temp'], function() {
     if(debug) {
-        gulp.src('./src/js/**/*.js', { base: src + 'js/' })
+        gulp.src('./src/js/**/*.js', { base: './src/js/' })
             .pipe(gulp.dest('./tmp/js/app'))
     }
     else {
@@ -70,18 +67,25 @@ gulp.task('build-vendor-js', ['clean-temp'], function() {
                     .pipe(plugins.uglify())
                 )
             .pipe(plugins.concat('vendor.min.js'))
-            .pipe(gulp.dest(tmp + 'js'))
+            .pipe(gulp.dest('./tmp/js'))
     }
 })
 
-gulp.task('copy-static', ['build-js'], function() {
-    return gulp.src(tmp + "**/*.*", { base: tmp })
-        .pipe(gulp.dest(stat))
+gulp.task('copy-vendor-font', function() {
+    return gulp.src(plugins.mainBowerFiles())
+            .pipe(plugins.filter('**/*.{eot,svg,ttf,woff}'))
+            .pipe(plugins.flatten())
+            .pipe(gulp.dest('./tmp/fonts'))
+})
+
+gulp.task('copy-static', ['build-js', 'copy-vendor-font'], function() {
+    return gulp.src('./tmp/**/*.*', { base: './tmp' })
+        .pipe(gulp.dest('./static'))
 })
 
 // Inject
 gulp.task('inject', ['clean-inject', 'copy-static'], function() {
-    jsFiles = [stat + 'js/vendor/*.js', stat + 'js/app/*.js']
+    jsFiles = ['./static/js/vendor/*.js', './static/js/app/*.js', './static/js/*.js']
     return gulp.src(templates + '**/*.html')
       .pipe(plugins.inject(gulp.src(jsFiles, {read: false}), {relative: false}))
       .pipe(gulp.dest(templates))
